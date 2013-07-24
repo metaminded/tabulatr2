83728c0a1ba6ed940f0b631ccf0dafd321320260
Florian Thomas <flo@florianthomas.net>
2013-07-24 13:01:14 +0200

    fix typo

--- lib/tabulatr/tabulatr/filter_icon.rb
+++ lib/tabulatr/tabulatr/filter_icon.rb
@@ -1,6 +1,6 @@
 class Tabulatr
   def render_filter_icon
-    make_tag(:a, class: 'icon-filter', href: '#tabulatr_filter_diialog',
+    make_tag(:a, class: 'icon-filter', href: '#tabulatr_filter_dialog',
       :'data-toggle' => 'modal'){}
   end
 end
