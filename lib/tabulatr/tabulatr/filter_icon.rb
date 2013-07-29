6893f0826677a6d4bf141be69882bb492eefc22e
Florian Thomas <flo@florianthomas.net>
2013-07-29 15:25:11 +0200

    update style

--- lib/tabulatr/tabulatr/filter_icon.rb
+++ lib/tabulatr/tabulatr/filter_icon.rb
@@ -1,6 +1,6 @@
 class Tabulatr
   def render_filter_icon
-    make_tag(:a, class: 'icon-filter', href: '#tabulatr_filter_dialog',
-      :'data-toggle' => 'modal'){}
+    make_tag(:a, class: 'btn btn-info', href: '#tabulatr_filter_dialog',
+      :'data-toggle' => 'modal'){ concat('<i class="icon-filter"></i>Filter') }
   end
 end
