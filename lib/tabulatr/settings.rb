7b6cec77312df60c49f885533c96413c110b0203
Florian Thomas <flo@florianthomas.net>
2013-10-17 12:13:35 +0200

    move table options to the right spot

--- lib/tabulatr/settings.rb
+++ lib/tabulatr/settings.rb
@@ -1,30 +1,8 @@
-#--
-# Copyright (c) 2010-2011 Peter Horn, Provideal GmbH
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#++
-
 require 'whiny_hash'
 
+module Tabulatr
+  module Settings
 
-class Tabulatr::Renderer
   # Hash keeping the defaults for the table options, may be overriden in the
   # table_for call
   TABLE_OPTIONS = WhinyHash.new({ # WhinyHash.new({
@@ -49,12 +27,6 @@ class Tabulatr::Renderer
     :footer_content => false,          # if given, add a <%= content_for <footer_content> %> before the </table>
     :path => '#'                       # where to send the AJAX-requests to
   })
-
-end
-
-module Tabulatr
-  module Settings
-
 
     # these settings are considered constant for the whole application, can not be overridden
     # on a per-table basis.
