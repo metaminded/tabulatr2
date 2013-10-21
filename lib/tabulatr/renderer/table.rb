5d30d61a1531365266f5c60bad912123d1bcbe4f
Florian Thomas <flo@florianthomas.net>
2013-10-21 14:13:17 +0200

    Updated README

--- lib/tabulatr/renderer/table.rb
+++ lib/tabulatr/renderer/table.rb
@@ -4,11 +4,7 @@ module Tabulatr
       def render_table(&block)
         to = @table_options[:table_html]
         table_id = "#{@klass.to_s.downcase}_table"
-<<<<<<< HEAD
         to = (to || {}).merge(:class => "tabulatr_table table",
-=======
-        to = (to || {}).merge(:class => "#{@table_options[:table_class]} table",
->>>>>>> 470130e... move count display over table, create new pagination trigger
           :'data-path' => @table_options[:path], :id => table_id)
         make_tag(:table, to) do
           make_tag(:thead) do
