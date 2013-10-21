ee257758bd90862cd96afd73984dbd21d454df57
Florian Thomas <flo@florianthomas.net>
2013-10-21 10:55:58 +0200

    improved css
    moved search field to the right

--- lib/tabulatr/renderer/search.rb
+++ lib/tabulatr/renderer/search.rb
@@ -4,14 +4,15 @@ module Tabulatr
 
       def render
         if(@table_options[:search])
-          make_tag(:form, class: "form-inline tabulatr-fuzzy-search", role: "form", id: "#{@klass.to_s.downcase}_fuzzy_search",
-                          :"data-table" => "#{@klass.to_s.downcase}_table") do
+          make_tag(:form, class: "form-inline tabulatr-fuzzy-search pull-right", role: "form",
+                      id: "#{@klass.to_s.downcase}_fuzzy_search",
+                      :"data-table" => "#{@klass.to_s.downcase}_table") do
             make_tag(:div, class: 'form-group')do
               make_tag(:label, class: 'sr-only', for: "#{@klass.to_s.downcase}_fuzzy_search_query") do
                 "search"
               end
-              make_tag(:input, class: "form-control",
-                        id: "#{@klass.to_s.downcase}_fuzzy_search_query", type: "search")
+              make_tag(:input, class: "form-control", placeholder: I18n.t('tabulatr.search'),
+                id: "#{@klass.to_s.downcase}_fuzzy_search_query", type: "search")
             end
           end
         end
