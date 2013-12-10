4498de289e447609cd4004dafdd9655ba0c2ea14
Florian Thomas <flo@florianthomas.net>
2013-12-10 23:54:35 +0100

    improve layout of generated tabulatr_data files

--- lib/tabulatr/generators/tabulatr/templates/tabulatr_data.rb
+++ lib/tabulatr/generators/tabulatr/templates/tabulatr_data.rb
@@ -1,18 +1,12 @@
-<% module_namespacing do %>
-class <%= class_name %>TabulatrData < Tabulatr::Data
-  <% attributes = attributes_names %>
-  <% if class_name.constantize.table_exists? %>
-    <% attributes << class_name.constantize.column_names.map(&:to_sym) %>
-  <% end %>
-  <% attributes = attributes.flatten.uniq %>
-  <% if attributes.any? %>
-    search :<%= attributes.last %>
-  <% end %>
-  <% attributes.each do |a| %>
-    column :<%= a %>
-  <% end %>
-  <% association_names.each do |attribute| %>
-    association :<%= attribute %>, :id
-  <% end %>
+<% module_namespacing do %>class <%= class_name %>TabulatrData < Tabulatr::Data
+<% attributes = attributes_names %><% if class_name.constantize.table_exists? %><% attributes << class_name.constantize.column_names.map(&:to_sym) %><% end %><% attributes = attributes.flatten.uniq %><% if attributes.any? %>
+  search :<%= attributes.last %>
+<% end %>
+<% attributes.each do |a| %>
+  column :<%= a %>
+<% end %>
+<% association_names.each do |attribute| %>
+  association :<%= attribute %>, :id
+<% end %>
 end
 <% end %>
