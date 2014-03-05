5d5a633810cf0b9008df47640fe7aacf2c22a454
Florian Thomas <flo@florianthomas.net>
2014-03-05 21:30:16 +0100

    fix broken build

--- spec/lib/tabulatr/renderer/renderer_spec.rb
+++ spec/lib/tabulatr/renderer/renderer_spec.rb
@@ -18,6 +18,7 @@ describe Tabulatr::Renderer do
       klass = Product
       view = double(render: nil)
       fake_block = Proc.new { }
+      Tabulatr::Renderer.main_klass = nil
       renderer = Tabulatr::Renderer.new(klass, view)
       expect{renderer.build_static_table([], &fake_block)}.to(
         change{Tabulatr::Renderer.main_klass}.from(nil).to(Product)
