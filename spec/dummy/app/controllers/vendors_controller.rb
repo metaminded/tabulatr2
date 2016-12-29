99902f61878491f62cafc59d4f22a630920c8915
Mikhail Titov <mlt@gmx.us>
2016-12-28 18:11:21 -0600

    Test controller variable access from filter

--- spec/dummy/app/controllers/vendors_controller.rb
+++ spec/dummy/app/controllers/vendors_controller.rb
@@ -1,5 +1,8 @@
 class VendorsController < ApplicationController
+  attr_reader :split
+
   def index
+     @split = params[:split] && params[:split].to_f || 100
      tabulatr_for Vendor
   end
 end
